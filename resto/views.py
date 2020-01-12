3b455015a92183b2ce8ffeeb76dce1fd9391a098
niEmerance <niyemera6@gmail.com>
2020-01-12 14:52:54 +0200

    creation of the Application

---
+++ resto/views.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')

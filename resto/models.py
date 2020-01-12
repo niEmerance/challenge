459ff120ea2c9119723faa916f78b7c258588ffa
niEmerance <niyemera6@gmail.com>
2020-01-12 15:39:55 +0200

    Adding Model

--- resto/models.py
+++ resto/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    resto_name = models.CharField(max_length =30)
+    resto_location = models.CharField(max_length =30)
+    resto_speciality= models.CharField(max_length =30)
+    resto_image = models.ImageField(upload_to = 'photos/')
+    resto_email = models.EmailField()
+
+    def __str__(self):
+        return self.resto_name
+
+    def save_Restaurant(self, user):
+        self.save()
